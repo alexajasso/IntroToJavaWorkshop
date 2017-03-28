7904b2878eb6af450ba3487563e8b567268170c9
alexajasso <league@WorthyMini-4.local>
2017-03-28 12:00:55 -0700

    We made a tortoise move like flash. We made a tortoise do 3d octagon.

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,22 @@
 package day1.robot;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
+		// START HERE
+		Robot r2d2 = new Robot();
+		r2d2.penDown();
+		r2d2.setPenColor(21, 54, 654);
+		r2d2.move(200);
+		r2d2.turn(90);
+		r2d2.move(200);
+		r2d2.turn(90);
+		r2d2.move(200);
+		r2d2.turn(90);
+		r2d2.move(200);
+
 	}
+
 }
