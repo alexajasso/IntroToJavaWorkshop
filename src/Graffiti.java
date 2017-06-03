52efbdf28126a04d9613fabd8aa12d11dd0a896d
alexajasso <league@Mac-mini-3.local>
2017-06-03 11:28:21 -0700

    We made a robot make a four square box.

---
+++ src/Graffiti.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+import sun.applet.Main;
+
+public class Graffiti {
+public static void main(String[] args) {
+	Robot r2d2=new Robot();
+	r2d2.setSpeed(10);
+	r2d2.setPenColor(Color.green);
+	r2d2.penDown();
+	r2d2.move(300);
+	r2d2.turn(90);
+	r2d2.move(300);
+	r2d2.turn(90);
+	r2d2.move(300);
+	r2d2.turn(90);
+	r2d2.move(300);
+	r2d2.move(300);
+	r2d2.turn(90);
+	r2d2.move(300);
+	r2d2.turn(90);
+	r2d2.move(300);
+	r2d2.move(300);
+	r2d2.turn(90);
+	r2d2.move(300);
+	r2d2.setPenColor(Color.MAGENTA);
+    r2d2.move(300);
+    r2d2.turn(90);
+    r2d2.move(300);
+    r2d2.turn(90);
+    r2d2.move(300);
+    r2d2.turn(-90);
+    r2d2.move(300);
+    r2d2.turn(-90);
+    r2d2.move(300);
+    r2d2.turn(-90);
+    r2d2.move(300);
+    r2d2.turn(-200);
+   
+    
+    
+    
+    
+}
+}
